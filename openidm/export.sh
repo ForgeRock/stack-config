39169429a774b806f9e8709c65e734cb74ede0fa
Warren Strange <warren.strange@gmail.com>
2016-03-16 10:09:14 -0600

    export

---
+++ openidm/export.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Example of how to export a remote server config
+/Users/warren/packages/openidm/cli.sh  configexport --url http://openam.example.com:30100/openidm -u openidm-admin:openidm-admin `pwd`/conf
