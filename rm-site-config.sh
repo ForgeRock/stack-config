ae371502405e843440177cbcb73fc7b470dcd74d
Warren Strange <warren.strange@forgerock.com>
2017-02-28 15:37:32 -0700

    README

---
+++ rm-site-config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# These files get created as part of the install process, so we can safely remove them from exported config
+# This helps to eliminate files that need the FQDN fixed up between environments.
+rm -f amster/global/Sites/site1.json
+rm -f amster/global/Servers/01/DirectoryConfiguration.json
+rm -f amster/global/Realms/root.json
+
+#rm -f amster/global/PasswordReset.json
