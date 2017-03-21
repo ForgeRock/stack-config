0541323721a91abce97912e9a8ce59c75496ef28
Warren Strange <warren.strange@forgerock.com>
2017-03-21 08:15:58 -0600

    openam export - mar 21

--- amster-clean.sh
+++ amster-clean.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+# Clean up amster exported files
 
 cd amster
-# Clean up amster exported files
 # These files get created as part of the install process, so we can safely remove them from exported config
 # This helps to eliminate files that need the FQDN fixed up between environments.
 rm -f global/Sites/site1.json
 rm -f global/Servers/01/DirectoryConfiguration.json
 rm -f global/Realms/root.json
+
 
 #rm -f global/PasswordReset.json
 
@@ -19,3 +20,7 @@ rm -f global/DefaultAdvancedProperties.json
 # Contains fqdn
 rm -f realms/root/BaseUrlSource.json
 rm -f realms/root/Policies/OAuth2ProviderPolicy.json
+
+
+rm -f realms/root/SunIdentityRepositoryService.json
+
